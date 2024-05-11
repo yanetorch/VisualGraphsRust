58fe5f6a72131204c23cca332c19180dd24d60c9
var4yn <2016slavyakinnikita@gmail.com>
2024-05-11 11:46:31 +0800

    init repo

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,17 @@
+mod tasks {
+
+}
+
+mod client_functions {
+
+}
+
+
+
+
+pub fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
